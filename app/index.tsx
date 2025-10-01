07650f35a95637bde714fb62bcfaf2632d8f52a5
guasatald <guasatald@gmail.com>
2025-10-01 16:55:01 +0000

    ✨feat(app): Instalacion de nativeWind y estilos de prueba -Creacion de CustomText - programacion del index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
-
+import CustomText from "@/components/ui/CustomText";
+import "@/global.css";
+import { View } from "react-native";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="bg-[#026A75] w-screen h-screen">
+      <CustomText variant="large" dark={false}>
+        Hello World
+      </CustomText>
     </View>
   );
 }
